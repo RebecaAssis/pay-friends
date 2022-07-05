0477899d703559e4bd2d796b37083ecbd2289f6a
Rebeca Assis <rebeca.assis@brainboss.com.br>
2022-07-05 00:02:09 -0300

    Added login and password verification.

--- src/app/public/login/login.component.ts
+++ src/app/public/login/login.component.ts
@@ -8,29 +8,38 @@ import { AuthenticationService } from '../services/authentication.service';
 })
 export class LoginComponent implements OnInit {
   srcPaths: string[] = ['../../../../assets/icons/visibility-off.svg', '../../../../assets/icons/visibility.svg'];
-
   loginForm = new FormGroup ({
     email: new FormControl(''),
     password: new FormControl('')
   });
-
   userAccount;
+  loginDataInserted;
+  authentication: boolean = false;
   
 
   constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
   }
+  
+  checkLoginData(dados) {
+    for (let key in dados) {
+      if(dados[key].email === this.loginDataInserted.email && dados[key].password === this.loginDataInserted.password) {
+        this.authentication = true;
 
-  onSubmit() {
-    console.log(this.loginForm.value)
-
-    this.authenticationService.getUser().subscribe((dados) => {
-      this.userAccount = dados;
-      console.log(this.userAccount)
-    });
-    
-    
+      } else {
+        this.authentication = false;
+      }
+    }
   }
+  
+  onSubmit() {
+    this.loginDataInserted = this.loginForm.value;
+    
+    this.authenticationService.getUser().subscribe((dados) => {
+      this.checkLoginData(dados);  
+    });
+  }
+  
 
 }
