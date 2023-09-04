2001e0b808029cd4b6e3e75f8476c31a20619363
Rebeca Assis <rebeca.assis@brainboss.com.br>
2023-09-03 21:09:10 -0300

    feat: created the input component and styled it.

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 import { ModalComponent } from './modal/modal.component';
+import { InputComponent } from './input/input.component';
 
 
 
 @NgModule({
   declarations: [
     ButtonComponent,
-    ModalComponent
+    ModalComponent,
+    InputComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     ButtonComponent,
-    ModalComponent
+    ModalComponent,
+    InputComponent
   ]
 })
 export class ComponentsModule { }
---
+++ src/app/shared/components/input/input.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss']
+})
+export class InputComponent implements OnInit {
+  @Input() label = 'Senha';
+  @Input() iconPath = '';
+  @Input() inputType = 'text';
+  @Output() iconEvent = new EventEmitter();
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  emitIconEvent () {
+    this.iconEvent.emit();
+  }
+
+}
