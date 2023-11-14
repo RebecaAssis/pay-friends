a9b53c279546485b1f5195c32d9b3c3c4f7f0495
Rebeca Assis <rebeca.oassis@hotmail.com>
2023-11-14 19:48:31 -0300

    feat: created the unit tests to the ModalComponent.

--- src/app/shared/components/modal/modal.component.spec.ts
+++ src/app/shared/components/modal/modal.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ModalComponent } from './modal.component';
 
 describe('ModalComponent', () => {
@@ -11,15 +10,47 @@ describe('ModalComponent', () => {
       declarations: [ ModalComponent ]
     })
     .compileComponents();
-  });
 
+  });
+  
   beforeEach(() => {
+    const propButtonIsDisabled = false;
+    function functionAsAInput () { 
+      return;
+    }
+
     fixture = TestBed.createComponent(ModalComponent);
     component = fixture.componentInstance;
+    component.buttonSubmitDisabled = propButtonIsDisabled;
+    component.submitFunction = functionAsAInput;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should emit the event when clicking the decline button', () => {
+    
+    //Arrange
+    const eventSpy = jest.spyOn(component.declineEvent, 'emit');
+
+    //Act
+    const declineButton = fixture.nativeElement.querySelector('.c-modal__content__footer__decline-btn');
+    declineButton.click();
+
+    //Assert
+    expect(eventSpy).toHaveBeenCalled();
+  });
+
+  it('should call the submitFunction when clicking the submite button', () => {
+    
+    //Arrange
+    const eventSpy = jest.spyOn(component, 'executeSubmitFunction');
+
+    //Act
+    const submitButton = fixture.nativeElement.querySelector('.c-modal__content__footer__submit-btn');
+    submitButton.click();
+
+    //Assert
+    expect(eventSpy).toHaveBeenCalled();
   });
 });
+
+
