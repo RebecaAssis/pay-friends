45fb1ced35c469180dcd2a34a5d02c6fdbaa95de
Rebeca Assis <rebeca.oassis@hotmail.com>
2023-11-16 14:52:46 -0300

    feat: created the tests to the roundPagesNumber and createdPagesLis functions.

--- src/app/private/components/steper/steper/steper.component.spec.ts
+++ src/app/private/components/steper/steper/steper.component.spec.ts
@@ -6,8 +6,8 @@ describe('SteperComponent', () => {
   let component: SteperComponent;
   let fixture: ComponentFixture<SteperComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(() => {
+    TestBed.configureTestingModule({
       declarations: [ SteperComponent ]
     })
     .compileComponents();
@@ -19,7 +19,54 @@ describe('SteperComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should assign the same pageList value to the viewPageList', () => {
+    //Arrange
+    component.pagesNumber = 5;
+
+    //Act
+    component.createdPagesList(component.pagesNumber);
+
+    //Assert
+    expect(component.viewPagesList).toEqual([1, 2, 3, 4, 5]);
+    expect(component.viewPagesList).toEqual(component.pageList);
+  });
+
+
+  it('should assign the first five items and the last item of the pageList to the viewPagesList array.', () => {
+    //Arrange
+    component.pagesNumber = 10;
+    
+    //Act
+    component.createdPagesList(component.pagesNumber);
+
+    //Assert
+    expect(component.viewPagesList).toEqual([1, 2, 3, 4, 5, 10]);
+  });
+
+  it('should round the page number value and to add with 1', () => {
+    //Arrange
+    let pagesNumber = 0;
+    component.pagesNumber = 10.5;
+
+    //Act
+    pagesNumber = component.roundPagesNumber(component.pagesNumber);
+
+    //Assert
+    expect(pagesNumber).toEqual(11);
+  });
+
+  it('should return the same value passed as parameter', () => {
+    //Arrange
+    let pagesNumber = 0;
+    component.pagesNumber = 9;
+
+    //Act
+    pagesNumber = component.roundPagesNumber(component.pagesNumber);
+
+    //Assert
+    expect(pagesNumber).toEqual(9);
   });
 });
+
+
+
