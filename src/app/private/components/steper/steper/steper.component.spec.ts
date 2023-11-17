6c12b908a905b33800dffdfde37593823d3d7d74
Rebeca Assis <rebeca.oassis@hotmail.com>
2023-11-17 17:59:38 -0300

    feat: created all the test cases to the SteperComponent functions.

--- src/app/private/components/steper/steper/steper.component.spec.ts
+++ src/app/private/components/steper/steper/steper.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SteperComponent } from './steper.component';
 
 describe('SteperComponent', () => {
@@ -19,6 +18,7 @@ describe('SteperComponent', () => {
     fixture.detectChanges();
   });
 
+  //createdPageList
   it('should assign the same pageList value to the viewPageList', () => {
     //Arrange
     component.pagesNumber = 5;
@@ -43,6 +43,7 @@ describe('SteperComponent', () => {
     expect(component.viewPagesList).toEqual([1, 2, 3, 4, 5, 10]);
   });
 
+  //roundPagesNumber
   it('should round the page number value and to add with 1', () => {
     //Arrange
     let pagesNumber = 0;
@@ -66,6 +67,55 @@ describe('SteperComponent', () => {
     //Assert
     expect(pagesNumber).toEqual(9);
   });
+
+  //previousPage
+  it('should update actualPage value to a value 1x smaller than the previous value and emit a event', () => {
+    component.pageList = [1, 2, 3, 4, 5, 34]
+    component.actualPage = 5;
+    const eventoSpy = jest.spyOn(component.EventActualPage, 'emit');
+
+    component.previousPage();
+
+    expect(component.actualPage).toEqual(4);
+    expect(eventoSpy).toHaveBeenCalled();
+  });
+
+  //nextPage
+  it('should update actualPage value to a value 1x bigger than the previous value and emit a event', () => {
+    component.pageList = [3, 4, 5, 6, 7, 34]
+    component.actualPage = 6;
+    const eventoSpy = jest.spyOn(component.EventActualPage, 'emit');
+
+    component.nextPage();
+
+    expect(component.actualPage).toEqual(7);
+    expect(eventoSpy).toHaveBeenCalled();
+  });
+
+  //updateViewPagesList
+  it('should update the actual viewPagesList to an array that each item is 1x bigger than previous items, and the last item must have the same value', 
+    () => {
+      component.viewPagesList = [2, 3, 4, 5, 6, 10];
+      component.pageList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      const param = '';
+
+      component.updateViewPagesList(param);
+
+      expect(component.viewPagesList).toEqual([1, 2, 3, 4, 5, 10]);
+    }
+  );
+
+  it('should update the actual viewPagesList to an array that each item is 1x smaller than previous items, and the last item must have the same value', 
+    () => {
+      component.viewPagesList = [1, 2, 3, 4, 5, 10];
+      component.pageList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      const param = 'next';
+
+      component.updateViewPagesList(param);
+
+      expect(component.viewPagesList).toEqual([2, 3, 4, 5, 6, 10]);
+    }
+  );
 });
 
 
